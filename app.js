bda36ba494471e13385ffbf5fd0756c680f1f109
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-31 09:09:30 +0500

    public file path has been added

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const app = express();
 app.use(express.json());
 
 app.use(
-  express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/'))
+);
+app.use(
+  express.static(path.join(__dirname, 'public'))
 );
 
 app.set('view engine', 'pug');
