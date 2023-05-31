52db8d66e1d0af220aabd1bd3eac6b968929d671
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-05-31 09:39:52 +0500

    starting point of creating viewRoute

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,3 +1,7 @@
+const Department = require('../models/departmentModel');
+const User = require('../models/userModel');
+const Document = require('../models/documentModel');
+
 exports.getEntryPoint = (req, res, next) => {
   res.status(200).render('index', {
     message: 'Welcome to e-archive',
