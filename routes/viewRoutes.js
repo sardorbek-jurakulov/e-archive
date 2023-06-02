e97e9acc901f76983978b8d6568a1cd5dc93b552
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-06-02 16:45:16 +0500

    WIP

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 
+
 const router = express.Router();
 
 router.get('/', viewController.redirectController);
