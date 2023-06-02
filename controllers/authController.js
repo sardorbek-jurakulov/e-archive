bff69cfc9214d7a1f30dd858c46b3f77b5c56a23
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2023-06-02 10:05:54 +0500

    WIP

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
